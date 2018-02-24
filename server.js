c3bd4322e6b1d3eba396113aebac33846f1b0b4b
khanxc <khan.kmk@gmail.com>
2018-02-24 23:18:39 +0530

    add sample console

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use((req,res, next) => {
   fs.appendFile('server.log', log + '\n', (err) => {
     if(err){
       console.log('Unable to append server log');
+      console.log('Sample error');
     }
   });
 
