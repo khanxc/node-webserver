8b1dd656498483e3dd284e52c7d5b4d9d4a0fcdf
khanxc <khan.kmk@gmail.com>
2018-02-24 16:44:41 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res, next) => {
+
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  
+  console.log(log);
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append server log');
+    }
+  });
+
+  next();
+});
+
+app.get('/', (req, res) => {
+
+//  res.send('<h2>Hello express!!!</h2>');
+res.send({
+  name:'Khan',
+  likes:['Biking','Hiking']
+});
+
+});
+
+app.get('/about', (req, res) => {
+
+res.render('about.hbs',{
+  pageTitle: 'About page',
+  currentear:new Date().getFullYear()
+});
+
+});
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server is up in port 3000');
+});
